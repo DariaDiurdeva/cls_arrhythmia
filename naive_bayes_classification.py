8761b9392ad2c3df01185116ccc535e74dde31ad
dasha <doshikdurdeva@gmail.com>
2020-02-11 00:50:22 +0500

    Revert "naive_Bayes_class is ready"

--- naive_bayes_classification.py
+++ naive_bayes_classification.py
@@ -1,3 +1,4 @@
+
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
@@ -5,15 +6,20 @@ from sklearn.naive_bayes import GaussianNB
 from data_preparation import prepare_data
 
 
+def classify(X, Y, test_size=0.30):
+    # разделим датасет на тренировочные и тестовые данные
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=23)
+    # создадим экземпляр класса классификатора GaussianNB
+    nb_model = GaussianNB()
+    # обучим классификатор с помощью функции fit на тренеровочных данных
+    nb_model.fit(x_train, y_train)
+    # применим классификатор к тестовым данных
+    prediction = nb_model.predict(x_test)
+    return prediction, y_test
+
+
 X, Y = prepare_data()
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.30,random_state=0)
-naiveBayes_model = GaussianNB()
-naiveBayes_model.fit(x_train, y_train)
-prediction = naiveBayes_model.predict(x_test)
+predict, real_values = classify(X, Y)
 
-print(y_test)
-print(prediction)
-
-print(accuracy_score(prediction, y_test))
-print(classification_report(prediction, y_test))
-
+print(accuracy_score(predict, real_values))
+print(classification_report(predict, real_values))
